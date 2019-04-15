24a7ab7b3185f315f4a5de458444babf1e5e0f70
jsanmigimeno <sanmiguelgimeno@hotmail.com>
2019-04-15 22:16:30 +0200

    Add matching calling functions

--- test.py
+++ test.py
@@ -78,13 +78,13 @@ if __name__ == '__main__':
         SSIM = model.get_SSIM()
         SSIM_total += SSIM
         matching = model.get_Matching()
-
+        matching_total += matching
 
         # save images to an HTML file
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-        webpage.add_text(("Losses - L1: %.4f, PSNR: %.4f, SSIM: %.4f" % (L1, PSNR, SSIM)))
+        webpage.add_text(("Losses - L1: %.4f, PSNR: %.4f, SSIM: %.4f, matching score: %.4f" % (L1, PSNR, SSIM, matching)))
 
     test_size = len(dataset)
     webpage.add_header("Overall performance")
-    webpage.add_text(("L1: %.4f, PSNR %.4f, SSIM: %.4f" % (L1_total/test_size, PSNR_total/test_size, SSIM_total/test_size)))    
+    webpage.add_text(("L1: %.4f, PSNR %.4f, SSIM: %.4f, matching score: %.4f" % (L1_total/test_size, PSNR_total/test_size, SSIM_total/test_size, matching_total/test_size)))    
     webpage.save()  # save the HTML
