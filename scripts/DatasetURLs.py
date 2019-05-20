8abddf97d5e05ac7759a36df1ecf845f2974d98f
jsanmigimeno <sanmiguelgimeno@hotmail.com>
2019-05-20 15:29:15 +0100

    Update urls to include man val set

--- scripts/DatasetURLs.py
+++ scripts/DatasetURLs.py
@@ -22,6 +22,8 @@ def getDataset(id):
         return 'https://imperiallondon-my.sharepoint.com/:u:/g/personal/js5415_ic_ac_uk/EQCOAp0ZMGVPghOYwa_nHdMB3Rk0FPrz1MMtVm2V2G9iQw?download=1'
     elif id == 'DownscaledJPEGSmall':
         return 'https://imperiallondon-my.sharepoint.com/:u:/g/personal/js5415_ic_ac_uk/ERZzyNbQEZdHh3IOpbeJnwIBTXKAmJKq-ZTIbhPWWMTuMw?download=1'
+    elif id == 'baselineScaledVal':
+        return 'https://imperiallondon-my.sharepoint.com/:u:/g/personal/js5415_ic_ac_uk/EQrXoEaDlU5Hptq72CdL1WwBxfa6HiaOimPhZpbIonp4Fg?download=1'
     else:
         return ''
 
